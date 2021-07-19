a86c8662175c8017106c4e71e4859a3a26a9d5af
ch4b13r <arpaxavier@gmail.com>
2021-07-19 16:18:10 +0200

    - Adding Test File

---
+++ Runtime/TestAttribute2.cs
@@ -0,0 +1,21 @@
+﻿#region Access
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Scripting;
+using UnityEditor;
+using XavHelpTo;
+#endregion
+
+//[Serializable]
+//[ClassInterface(ClassInterfaceType.None)]
+//[ComDefaultInterface(typeof(_Attribute))]
+//[ComVisible(true)]
+[AttributeUsage(AttributeTargets.Field, Inherited = true, AllowMultiple = false)]
+//[AttributeUsage(AttributeTargets.Class, Inherited = true, AllowMultiple = false)]
+//[AttributeUsage(AttributeTargets.Method, Inherited = true, AllowMultiple = false)]
+public class TestAttribute2: PropertyAttribute{
+    public TestAttribute2() => "Test !".Print("null");
+    public TestAttribute2(string color) => "Test !".Print(color);
+}
